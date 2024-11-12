8800769e386506a6176e932061b75ba24c1f63a8
Edison <edhellslpa@gmail.com>
2024-11-12 17:20:34 -0500

    Dockerizacion

--- Jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: 'jsdom',
-  moduleDirectories: ['node_modules', 'pages'],
-  testMatch: ['**/?(*.)+(test).js?(x)'],
-};
